515eab021efc21ac3da4fb11bf0dcaa128bb727b
G4 <gabriel.dev0105@gmail.com>
2023-04-10 21:39:07 -0300

    create header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
+import { Center, Image } from "@chakra-ui/react"
+import logo from '/logo.svg'
 export const Header = () => {
   return (
     <div>
-      Logo
+      <Center bg='#0478D1' h='100px' color='white'>
+        <Image src={logo} alt='logo-mateus' />
+      </Center>
     </div>
   )
 }
