624bfa6f94c299cfa682c74b5d4b3671cf381fd1
G4 <gabriel.dev0105@gmail.com>
2023-04-19 20:36:04 -0300

    "create structure body"

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,7 +1,73 @@
+import { Box, Button, Center, Checkbox, Heading, Input, Stack, Textarea, VStack } from "@chakra-ui/react"
+
 export const Body = () => {
   return (
-    <div>
-      Body
-    </div>
+    <VStack spacing={4}>
+      <Heading as='h3' size='lg' my='16px'>
+        Tipo de Precificação
+      </Heading>
+      <Stack direction='row' spacing={4} align='center'>
+        <Button colorScheme='green' variant='solid'>
+          Regular
+        </Button>
+        <Button colorScheme='green' variant='solid'>
+          Promoção
+        </Button>
+      </Stack>
+      <Stack direction='row' spacing={32} align='center'>
+      <Heading as='h3' size='mg'>
+        Início
+      </Heading>
+      <Heading as='h3' size='mg'>
+        Final
+      </Heading>
+      </Stack>
+      <Stack direction='row' spacing={4} align='center'>
+      <Input
+        placeholder="Select Date and Time"
+        size="md"
+        type="date"
+      />
+      <Input
+        placeholder="Select Date and Time"
+        size="md"
+        type="date"
+      />
+      </Stack>
+      <Heading as='h3' size='mg'>
+        Emrpesa
+      </Heading>
+      <Stack spacing={8} direction='row'>
+        <Checkbox colorScheme='green' defaultChecked>
+          2 (Grupo Mateus)
+        </Checkbox>
+        <Checkbox colorScheme='green' defaultChecked>
+          4 (Armazém)
+        </Checkbox>
+        <Checkbox colorScheme='green' defaultChecked>
+          15 (Franquias)
+        </Checkbox>
+      </Stack>
+      <Heading as='h3' size='mg'>
+        Regional
+      </Heading>
+      <Stack spacing={8} direction='row'>
+        <Checkbox colorScheme='green' defaultChecked>
+          Capital
+        </Checkbox>
+        <Checkbox colorScheme='green' defaultChecked>
+          Norte
+        </Checkbox>
+        <Checkbox colorScheme='green' defaultChecked>
+          Sul
+        </Checkbox>
+        <Checkbox colorScheme='green' defaultChecked>
+          Piauí
+        </Checkbox>
+      </Stack>
+      <Center w='100%'>
+        <Textarea w='30%' placeholder='Importar produtos e preços' />
+      </Center>
+    </VStack>
   )
 }
